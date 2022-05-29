dc7d252c278ea4544ad7bc84c54462aa08b88c85
Mattias <mattias@mattiasgustavsson.com>
2022-05-30 01:32:53 +0200

    Displaying yarnspin logo

--- source/yarnspin.c
+++ source/yarnspin.c
@@ -10,10 +10,60 @@
 #include "libs/stb_image.h"
 
 
+APP_U32 blend( APP_U32 x, APP_U32 y, APP_U32 a ) {
+    APP_U32 xr = ( x ) & 0xff;
+    APP_U32 xg = ( x >> 8 ) & 0xff;
+    APP_U32 xb = ( x >> 16 ) & 0xff;
+    APP_U32 yr = ( y ) & 0xff;
+    APP_U32 yg = ( y >> 8 ) & 0xff;
+    APP_U32 yb = ( y >> 16 ) & 0xff;
+    APP_U32 ia = 255 - ( a & 0xff );
+    APP_U32 r = ( xr * ia + yr * a ) >> 8;
+    APP_U32 g = ( xg * ia + yg * a ) >> 8;
+    APP_U32 b = ( xb * ia + yb * a ) >> 8;
+    return r | ( g << 8 ) | ( b << 16 );
+}
+
+
 int app_proc( app_t* app, void* user_data ) {
     (void) user_data;
 
+    // position window centered on main display
+    app_displays_t displays = app_displays( app );
+    if( displays.count > 0 ) {
+        // find main display
+        int disp = 0;
+        for( int i = 0; i < displays.count; ++i ) {
+            if( displays.displays[ i ].x == 0 && displays.displays[ i ].y == 0 ) {
+                disp = i;
+                break;
+            }
+        }
+        // calculate aspect locked width/height
+        int scrwidth = displays.displays[ disp ].width - 80;
+        int scrheight = displays.displays[ disp ].height - 80;
+        int aspect_width = (int)( ( scrheight * 4.25f ) / 3 );
+        int aspect_height = (int)( ( scrwidth * 3 ) / 4.25f );
+        int target_width, target_height;
+        if( aspect_height <= scrheight ) {
+            target_width = scrwidth;
+            target_height = aspect_height;
+        } else {
+            target_width = aspect_width;
+            target_height = scrheight;
+        }
+        // set window size and position
+        int x = displays.displays[ disp ].x + ( displays.displays[ disp ].width - target_width ) / 2;
+        int y = displays.displays[ disp ].y + ( displays.displays[ disp ].height - target_height ) / 2;
+        int w = target_width;
+        int h = target_height;
+        app_window_pos( app, x, y );
+        app_window_size( app, w, h );
+    }
     
+    app_screenmode( app, APP_SCREENMODE_WINDOW );
+    app_title( app, "Yarnspin" );
+
     crtemu_pc_t* crtemu = crtemu_pc_create( NULL );
     int w, h, c;
     stbi_uc* crtframe = stbi_load( "images/crtframe.png", &w, &h, &c, 4 );
@@ -24,18 +74,22 @@ int app_proc( app_t* app, void* user_data ) {
     frametimer_lock_rate( frametimer, 60 );
 
     static APP_U32 canvas[ 320 * 200 ];
-    memset( canvas, 0xC0, sizeof( canvas ) );
-    app_screenmode( app, APP_SCREENMODE_WINDOW );
+    memset( canvas, 0, sizeof( canvas) );
 
+    // display yarnspin logo
+    APP_U32* logo = (APP_U32*) stbi_load( "images/yarnspin_logo.png", &w, &h, &c, 4 );
+    for( int i = 0; i < 320 * 200; ++i ) {
+        canvas[ i ] = blend( canvas[ i ], logo[ i ], logo[ i ] >> 24 );
+    }
+    stbi_image_free( logo );
+
+    // main loop
     while( app_yield( app ) != APP_STATE_EXIT_REQUESTED ) {
         frametimer_update( frametimer );
-        int x = rand() % 320;
-        int y = rand() % 200;
-        APP_U32 color = rand() | ( (APP_U32) rand() << 16 );
-        canvas[ x + y * 320 ] = color;
         crtemu_pc_present( crtemu, 0, canvas, 320, 200, 0xffffff, 0x000000 );
         app_present( app, NULL, 1, 1, 0xffffff, 0x000000 );
     }
+
     frametimer_destroy( frametimer );
     crtemu_pc_destroy( crtemu );
     return 0;
