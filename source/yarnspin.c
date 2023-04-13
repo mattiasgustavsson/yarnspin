ab15c63fcf3f8cdbf395c74538b40458b81f6747
Mattias Gustavsson <mattias@mattiasgustavsson.com>
2023-04-13 22:20:01 +0200

    Added save functionality via web storage

--- source/yarnspin.c
+++ source/yarnspin.c
@@ -72,6 +72,8 @@ char const* get_executable_filename( void );
 void* compress_lzma( void* data, size_t size, size_t* out_size );
 size_t decompress_lzma( void* compressed_data, size_t compressed_size, void* buffer, size_t size );
 
+bool save_data( char const* name, void* data, size_t size );
+void* load_data( char const* name, size_t* out_size );
 
 // automatic memory management helper stuff
 
@@ -1081,8 +1083,211 @@ void ensure_console_open( void ) {
 #endif
 
 
+#ifdef __wasm__
 
-#ifdef _WIN32
+    char* base64enc( unsigned char const* data, size_t input_length, size_t* out_size ) {
+        static const char encoding_table[] = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', '0', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '-', '_'
+        };
+
+        const int mod_table[] = { 0, 2, 1 };
+
+        size_t output_length = 4 * ( ( input_length + 2 ) / 3 );
+
+        char* encoded_data = (char*) malloc( output_length );
+
+        if( encoded_data == NULL ) {
+            return NULL;
+        }
+
+        for( int i = 0, j = 0; i < input_length; ) {
+
+            uint32_t octet_a = i < input_length ? (unsigned char) data[ i++ ] : 0;
+            uint32_t octet_b = i < input_length ? (unsigned char) data[ i++ ] : 0;
+            uint32_t octet_c = i < input_length ? (unsigned char) data[ i++ ] : 0;
+
+            uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+            encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+            encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+            encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+            encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+
+        }
+
+        for( int i = 0; i < mod_table[ input_length % 3 ]; ++i ) {
+            encoded_data[ output_length - 1 - i ] = '=';
+        }
+
+        output_length = output_length - 2 + mod_table[ input_length % 3 ];
+        encoded_data[ output_length ] = 0;
+        if( out_size ) {
+            *out_size = output_length;
+        }
+        return encoded_data;
+
+    }
+
+    void* base64dec( char const* data, size_t input_length, size_t* out_size ) {
+        static const unsigned char decoding_table[256] = {
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x3e,0x00,0x00,0x34,0x35,0x36,0x37,0x38,0x39,0x3a,0x3b,0x3c,
+            0x3d,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,
+            0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x00,0x00,0x00,0x00,
+            0x3f,0x00,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,0x29,0x2a,
+            0x2b,0x2c,0x2d,0x2e,0x2f,0x30,0x31,0x32,0x33,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 
+        };
+
+        if( input_length % 4 != 0 ) {
+            return NULL;
+        }
+
+        size_t output_length = input_length / 4 * 3;
+
+        if( data[ input_length - 1 ] == '=') output_length--;
+        if( data[ input_length - 2 ] == '=') output_length--;
+
+        unsigned char* decoded_data = (unsigned char*)malloc( output_length + 1 );
+
+        if( decoded_data == NULL ) {
+            return NULL;
+        }
+
+        for( int i = 0, j = 0; i < input_length; ) {
+
+            uint32_t sextet_a = data[ i ] == '=' ? 0 & i++ : decoding_table[ data[ i++ ] ];
+            uint32_t sextet_b = data[ i ] == '=' ? 0 & i++ : decoding_table[ data[ i++ ] ];
+            uint32_t sextet_c = data[ i ] == '=' ? 0 & i++ : decoding_table[ data[ i++ ] ];
+            uint32_t sextet_d = data[ i ] == '=' ? 0 & i++ : decoding_table[ data[ i++ ] ];
+
+            uint32_t triple = 
+                  ( sextet_a << 3 * 6 )
+                + ( sextet_b << 2 * 6 )
+                + ( sextet_c << 1 * 6 )
+                + ( sextet_d << 0 * 6 );
+
+            if( j < output_length ) { decoded_data[ j++ ] = ( triple >> 2 * 8 ) & 0xFF; }
+            if( j < output_length ) { decoded_data[ j++ ] = ( triple >> 1 * 8 ) & 0xFF; }
+            if( j < output_length ) { decoded_data[ j++ ] = ( triple >> 0 * 8 ) & 0xFF; }
+        }
+
+        decoded_data[ output_length ] = 0;
+
+        if( out_size ) {
+            *out_size = output_length;
+        }
+
+        return decoded_data;
+
+    };
+
+    WAJIC(bool, web_storage_save, ( char const* name, char const* value ), {
+        try {
+            window.localStorage.setItem( MStrGet( name ), MStrGet( value ) );        
+        } catch {
+            return false;
+        }
+        return true;
+    })
+    
+    WAJIC(char*, web_storage_load, ( char const* name ), {
+        var value = window.localStorage.getItem( MStrGet( name ) );
+        if( value == null ) {
+            return null;
+        }
+        return MStrPut( value );
+    })
+
+    bool save_data( char const* name, void* data, size_t size ) {
+        char* str = base64enc( data, size, NULL );
+        web_storage_save( name, str );
+        free( str );
+        return false;
+    }
+
+    void* load_data( char const* name, size_t* out_size ) {        
+        char* value = web_storage_load( name );
+        if( !value ) {
+            return NULL;
+        }
+        size_t size = 0;
+        void* data = base64dec( value, strlen( value ), &size );
+        free( value );
+        if( data && out_size ) {
+            *out_size = size;
+        }
+        return data;
+    }
+
+#else
+
+    bool save_data( char const* name, void* data, size_t size ) {
+        if( !folder_exists( "savegame" ) ) {
+            makedir( "savegame" );
+        }
+        cstr_t filename = cstr_cat( "savegame/", name );
+        FILE* fp = fopen( filename, "wb" );
+        if( !fp ) {
+            return false;
+        }
+        size_t size_written = fwrite( data, 1, size, fp );
+        fclose( fp );
+        if( size_written != size ) {
+            return false;
+        }
+        return true;
+    }
+
+
+    void* load_data( char const* name, size_t* out_size ) {      
+        if( !folder_exists( "savegame" ) ) {
+            return NULL;
+        }
+        cstr_t filename = cstr_cat( "savegame/", name );
+        FILE* fp = fopen( filename, "rb" );
+        if( !fp ) {
+            return NULL;
+        }
+        fseek( fp, 0, SEEK_END );
+        size_t size = ftell( fp );
+        fseek( fp, 0, SEEK_SET );
+        if( size == 0 ) {
+            fclose( fp );
+            return NULL;
+        }
+        void* data = malloc( size + 1 );
+        size_t size_read = fread( data, 1, size, fp );
+        fclose( fp );
+        if( size_read != size ) {
+            free( data );
+            return NULL;
+        }
+        ((char*)data)[ size ] = '\0';
+        if( out_size ) {
+            *out_size = size;
+        }
+        return data;
+    }
+
+#endif
+
+
+    #ifdef _WIN32
 
     int opengl_preinit_thread_proc( void* user_data ) {
         (void) user_data;
